7e9db96d2f3ff79c3d5664eece5497ffbd843494
diddy <kantohenryvaliant@gmail.com>
2026-03-07 18:16:17 +0100

    Update void.lua

--- void.lua
+++ void.lua
@@ -1,3 +1,4 @@
+--[""] = true/false,
 return {
     ["darkgamer_alt354"] = true,
     ["17K_xVibezzzz"] = true,
@@ -7,4 +8,5 @@ return {
     ["CryForCyr0"] = true,
     ["Test002533533"] = true,
     ["LockZzScm"] = true,
+    ["BuffetDragon228_2"] = true,
 }
