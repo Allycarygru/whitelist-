9abaf4abdb211f862fb6ede41b08ac2391892d5b
diddy <kantohenryvaliant@gmail.com>
2026-03-15 20:17:23 +0100

    Update void.lua

--- void.lua
+++ void.lua
@@ -6,7 +6,7 @@ return {
     ["17K_xVibezzzz"] = true,
     ["Lockzzscm"] = true,
     ["WhyImPlayThisGame666"] = true,
-    ["78cvzl"] = true,
+    --["78cvzl"] = true,
     ["CryForCyr0"] = true,
     ["Test002533533"] = true,
     ["LockZzScm"] = true,
